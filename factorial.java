da9305b98ad8215ba3972b60ba28edf0fd3f8bc2
Raghu Ram <117032033+Raghu711p@users.noreply.github.com>
2025-07-08 15:59:43 +0530

    Java factorial using recursion

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+  public static int fact(int n){
+     if (n ==0 || n==1)
+         return 1;
+    else {
+        return n*fact(n-1);
+     }
+  }
+
+    public static void main(String[] args) {
+        int result=fact(4);
+        System.out.println(result);
+    }
+}
